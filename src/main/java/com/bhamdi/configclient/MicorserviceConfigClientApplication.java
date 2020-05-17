399c48b042b18c46554222f609a653f514cad282
Beligh Hamdi <beligh.hamdi@gmail.com>
2020-05-17 00:13:20 -0400

    client config

---
+++ src/main/java/com/bhamdi/configclient/MicorserviceConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.bhamdi.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicorserviceConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicorserviceConfigClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhamdi/configclient/MicorserviceConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhamdi.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicorserviceConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
